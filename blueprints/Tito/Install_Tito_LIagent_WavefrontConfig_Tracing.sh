f298ab812089d30e581eff213727261f815cc3f8
ahugla <ahugla@vmware.com>
2020-01-27 13:32:37 +0100

    Update Install_Tito_LIagent_WavefrontConfig_Tracing.sh

--- blueprints/Tito/Install_Tito_LIagent_WavefrontConfig_Tracing.sh
+++ blueprints/Tito/Install_Tito_LIagent_WavefrontConfig_Tracing.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
-# INSTALL TITO SUR CentOS7.x
-#----------------------------------------------------------------------------------
+# INSTALL TITO SUR CentOS7.x:
+#    - httpd
+#    - Tito App
+#	 - Wavefront metriques et Tracing
+#    - Log Insight Agent
+#-------------------------------------------------------------------------------------------------
 # alex
-# 3/01/2020
+# 27/01/2020
 #
-# V1.0
+# V2.0
 #
-#----------------------------------------------------------------------------------
-# USAGE
+#-------------------------------------------------------------------------------------------------
+# USAGE:
 #
-# Install_Tito_LIagent_WavefrontConfig.sh  [WAVEFRONT PROXY FQDN]  [WAVEFRONT PORT]
+# Install_Tito_LIagent_WavefrontConfig_Tracing.sh  [WAVEFRONT PROXY FQDN]  [WAVEFRONT PORT] [TITO VERSION]
 #
-#----------------------------------------------------------------------------------
+#     ex: .Install_Tito_LIagent_WavefrontConfig_Tracing.sh   172.19.5.4  2878   V1.9.6
+#-------------------------------------------------------------------------------------------------
 
 # Get and display parameters
 PROXY_NAME=$1
 PROXY_PORT=$2
+TITO_VERSION=$3
 echo "PROXY_NAME=$PROXY_NAME"
 echo "PROXY_PORT=$PROXY_PORT"
+echo "TITO_VERSION=$TITO_VERSION"
 
+
+# Install web server 
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+yum --enablerepo=remi,remi-php72 install -y httpd php php-common
 
 
 # Install Python3 and libraries
@@ -29,22 +41,18 @@ pip3 install wavefront-opentracing-sdk-python --no-cache-dir
 
 
 # TITO INSTALL
-cd /tmp
-rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
-yum --enablerepo=remi,remi-php72 install -y httpd php php-common
 git clone https://github.com/vmeoc/Tito.git  /var/www/html
 cd /var/www/html
-git checkout V1.9.6
+git checkout $TITO_VERSION
 
   
+# Rendre executable le script Python de Tracing
+chmod 777 /var/www/html/sendTraces.py
+
+
 # config httpd.conf pour autoriser le lancement de scripts
 sed -i '/<Directory "\/var\/www\/html">/a AddHandler cgi-script .cgi .pl .py' /etc/httpd/conf/httpd.conf
 sed -i '/<Directory "\/var\/www\/html">/a Options +ExecCGI' /etc/httpd/conf/httpd.conf
-
-
-# Rendre executable le script Python de Tracing
-chmod 777 /var/www/html/sendTraces.py
 
 
 # WAVEFRONT CONFIG - set variable for apache in "/etc/sysconfig/httpd"
