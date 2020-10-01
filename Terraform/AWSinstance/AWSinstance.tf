692a25231990459ace5b7b499dc39a7537921049
ahugla <ahugla@vmware.com>
2020-10-01 10:05:38 +0200

    Update AWSinstance.tf

--- Terraform/AWSinstance/AWSinstance.tf
+++ Terraform/AWSinstance/AWSinstance.tf
@@ -1,16 +1,10 @@
-variable "access_key" {
-  description = "AWS Access Key"
-}
-
-
-variable "secret_key" {
-  description = "AWS Secret Key"
-}
-
-
 provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
+}
+
+
+variable "myTag" {
+  description = "My Input Tag"
+  default = "alextag"
 }
 
 
@@ -19,8 +13,7 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
 
   tags = {
-    TagKey1 = "MonSuperTag"
+  "type" = var.myTag
   }
-
+  
 }
-
