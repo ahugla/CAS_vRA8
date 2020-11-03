370d80152098c6f12000f9e8d8c9fb28b833b4de
ahugla <ahugla@vmware.com>
2020-11-03 18:00:15 +0100

    Update AWSinstance.tf

--- Terraform/AWSinstance/AWSinstance.tf
+++ Terraform/AWSinstance/AWSinstance.tf
@@ -9,8 +9,8 @@ variable "myTag" {
 
 
 resource "aws_instance" "web" {
-  ami = "ami-08a2aed6e0a6f9c7d"
-  instance_type = "t2.micro"
+  ami = "ami-0950a18001c172f3a"
+  instance_type = "t2.nano"
 
   tags = {
   "type" = var.myTag
