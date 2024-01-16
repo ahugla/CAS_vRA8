f347d2b2510b1a88f06b7886bb5c325e33fb1ce8
ahugla <ahugla@vmware.com>
2024-01-16 14:23:43 +0100

    nextcloud 3 tiers

---
+++ blueprints/nextcloud_with_minio_3tiers/install_minio_for_nextcloud.sh
@@ -0,0 +1,208 @@
+#   INSTALLATION DE MINIO SUR ROCKY9
+#
+#  v1.0
+#
+#  Alexandre Hugla
+#  03/08/2023
+#
+#
+#  USAGE :  install_minIO.sh [password] [redis_password]
+#
+#  ACCESS : IP:9000 avec le compte "minioadmin"
+#
+
+
+# Recuperation des variables
+MinIO_password=$1
+redis_password=$2
+
+
+# Set parameters
+redisServer=vra-009429.cpod-vrealize.az-fkd.cloud-garage.net
+redisPort=6379
+export MINIO_ACCESS_KEY=minioadmin
+export MINIO_VOLUMES="/data"
+export MINIO_OPTS="--address :9000"
+DomainName=cpod-vrealize.az-fkd.cloud-garage.net
+minio_FQDN=$HOSTNAME.$DomainName                    #  FQDN du serveur lui meme (minio)
+
+
+
+cd /tmp
+yum install -y wget
+
+
+# create partition
+(echo "n"; echo "p"; echo ""; echo ""; echo ""; echo "w") | fdisk /dev/sdb 
+#'lsblk' pour voir
+
+# format partition
+mkfs -t ext4 /dev/sdb1
+#'lsblk -f'  pour voir
+
+# folder for minio storage file
+mkdir /data
+
+# mount 
+mount /dev/sdb1 /data
+# mount permanent (reboot persistent)
+echo "/dev/sdb1               /data         ext4    defaults        0   0"   >> /etc/fstab
+systemctl daemon-reload
+
+
+#download minIO
+mkdir /opt/minio
+mkdir /opt/minio/bin
+wget https://dl.minio.io/server/minio/release/linux-amd64/minio -O /opt/minio/bin/minio
+chmod +x /opt/minio/bin/minio
+
+
+# create content bucket
+mkdir /data/nextcloud
+
+
+# minIO config
+cat >/opt/minio/minio.conf <<EOF
+MINIO_VOLUMES=$MINIO_VOLUMES
+MINIO_OPTS=$MINIO_OPTS
+MINIO_ACCESS_KEY=$MINIO_ACCESS_KEY
+MINIO_SECRET_KEY=$MinIO_password
+EOF
+
+
+
+
+# create service
+cat >/etc/systemd/system/minio.service <<EOF
+[Unit]
+Description=Minio
+Wants=network-online.target
+After=network-online.target
+AssertFileIsExecutable=/opt/minio/bin/minio
+
+[Service]
+WorkingDirectory=/root
+User=root
+Group=root
+
+PermissionsStartOnly=true
+EnvironmentFile=/opt/minio/minio.conf
+ExecStartPre=/bin/bash -c "[ -n \"${MINIO_VOLUMES}\" ] || echo \"Variable MINIO_VOLUMES not set in /opt/minio/minio.conf\""
+
+ExecStart=/opt/minio/bin/minio server ${MINIO_OPTS} ${MINIO_VOLUMES}
+
+StandardOutput=journal
+StandardError=inherit
+
+# Specifies the maximum file descriptor number that can be opened by this process
+LimitNOFILE=65536
+
+# Disable timeout logic and wait until process is stopped
+TimeoutStopSec=0
+
+# SIGTERM signal is used to stop Minio
+KillSignal=SIGTERM
+SendSIGKILL=no
+SuccessExitStatus=0
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload and start service
+systemctl daemon-reload
+systemctl start minio
+systemctl enable minio
+
+
+# Install le commande line mc, l'equivalent de kubectl pour minio
+curl https://dl.min.io/client/mc/release/linux-amd64/mc  --create-dirs -o $HOME/minio-binaries/mc
+chmod +x $HOME/minio-binaries/mc
+export PATH=$PATH:$HOME/minio-binaries/
+
+
+# configurer l'alias 'local' de mc  (equivalent d'un configfile pour K8S) pour donner les droits d'acces
+# local est l'alias utilisé (le contexte qui contient les credentials), il existe par defaut et pointe vers le s3 minio local
+#mc alias set ALIAS URL ACCESSKEY SECRETKEY
+mc alias set local http://localhost:9000 $MINIO_ACCESS_KEY $MinIO_password  
+#mc admin info local   #verification
+
+     
+#create policy   "./policyminio.json":
+cat <<EOF > ./policyminio.json
+{
+ "Version": "2012-10-17",
+ "Statement": [
+  {
+   "Effect": "Allow",
+   "Action": [
+    "admin:*"
+   ]
+  },
+  {
+   "Effect": "Allow",
+   "Action": [
+    "kms:*"
+   ]
+  },
+  {
+   "Effect": "Allow",
+   "Action": [
+    "s3:*"
+   ],
+   "Resource": [
+    "arn:aws:s3:::*"
+   ]
+  }
+ ]
+}
+EOF
+
+
+# creer un token pour minioadmin qu'on met dans /root/minioTokenForNextcloud
+# ---------------------------------------------------------------------------
+# mc admin user svcacct add  --access-key "myuserserviceaccount"   --secret-key "myuserserviceaccountpassword"  --policy "/path/to/policy.json"   alias  user
+# local est l'alias utilisé (le contexte qui contient les credentials), il existe par defaut et pointe vers le s3 minio local
+echo `mc admin user svcacct add --policy ./policyminio.json local minioadmin` > /root/minioTokenForNextcloud
+    #=>   Access Key: 8D2JSDWEQJY93V7JGK96 Secret Key: zVfI4HrSKB2+azu96q3D97cow0QXQE5Wgr88k2d6 Expiration: no-expiry
+
+# une fois la policy créee on peut supprimer le fichier
+rm -f ./policyminio.json
+
+
+
+# On met sur Redis le compte pour minio
+# --------------------------------------
+# retrieve minio Access Key and Secret Key
+Access_Key=`more /root/minioTokenForNextcloud  | awk '{print $3}'`
+Secret_Key=`more /root/minioTokenForNextcloud  | awk '{print $6}'`
+# mise a disposition de minio Access Key / Secret Key via un redis central
+dnf install -y redis   # pour avoir redis-cli
+redis_auth=" -h $redisServer -p $redisPort --user dbadmin --pass $redis_password "
+cmd1=" set  Minio_Access_Key_$HOSTNAME  $Access_Key  EX 1200 "    # supprimé de redis apres 1200 sec (temporaire)
+redis-cli $redis_auth $cmd1
+cmd2=" set  Minio_Secret_Key_$HOSTNAME  $Secret_Key  EX 1200 "    # supprimé de redis apres 1200 sec (temporaire)
+redis-cli $redis_auth $cmd2
+
+
+
+
+# Configuration de l'acces en HTTPS:
+# ---------------------------------
+# By default, the MinIO server looks for the TLS keys and certificates, mais si absent fonctionne en HTTP
+# Le certif est dans  :   /root/.minio/certs
+
+# Generate a self-signed certificate 
+cd /root/.minio/certs
+openssl req -x509 -sha256 -nodes -days 3650 -newkey rsa:2048 -keyout private.key -out public.crt -subj "/C=XX/ST=FR/L=Paris/O=Broadcom/OU=CMPSE/CN=$minio_FQDN"
+# crée : "private.key" et "public.crt"
+
+#redemarrage de minio
+systemctl restart minio
+
+
+
+
+
+# nettoyage
+dnf remove -y redis    # plus besoin
---
+++ blueprints/nextcloud_with_minio_3tiers/install_nextcloud.sh
@@ -0,0 +1,290 @@
+#!/bin/bash
+
+
+#
+#
+#   Install LAMP stack with mariaDB
+#
+#
+#   USAGE : ./script.sh  [DB_root_password]  [DB_nextcloud_user_password]  [minio_server_IP]  [minio_server_Hostname] [minio_root_password]  [nextcloud_admin_password]  [redis_password]  [loglevel]
+#
+#
+#   ACCESS : IP/nextcloud avec le compte "admin" 
+#	
+#   https://wiki.crowncloud.net/?How_to_Install_LAMP_Stack_on_Rocky_Linux_9
+#   https://wiki.crowncloud.net/?How_to_Install_NextCloud_on_Rocky_Linux_9
+#   https://docs.nextcloud.com/server/latest/admin_manual/configuration_server/automatic_configuration.html
+#
+
+
+
+# Recuperation des variables
+# --------------------------
+DB_root_password=$1
+#echo "DB_root_password = " $DB_root_password					        # full admin sur la DB
+DB_nextcloud_user_password=$2
+#echo "DB_nextcloud_user_password = " $DB_nextcloud_user_password   # compte qui a les droits sur la DB nextcloud
+minio_server_IP=$3
+minio_server_Hostname=$4
+minio_root_password=$5
+nextcloud_admin_password=$6
+#echo "nextcloud_admin_password = " $nextcloud_admin_password       # compte admin de nextcloud (UI)
+redis_password=$7                                                   # password de la base externe redis dans laquelle on a mis le accessKey/secretKey pour minio
+loglevel=$8                                                       # loglevel ('debug', 'info' ou 'error') . Impacte httpd et nextcloud
+
+
+
+# parametres
+# ----------
+redisServer=vra-009429.cpod-vrealize.az-fkd.cloud-garage.net
+redisPort=6379
+DomainName=cpod-vrealize.az-fkd.cloud-garage.net
+nextcloud_FQDN=$HOSTNAME.$DomainName                                # FQDN du serveur local (nextcloud)
+nextcloud_IP=$(hostname  -I | cut -f1 -d' ')                        # IP du serveur local (nextcloud)
+minio_FQDN=$minio_server_Hostname.$DomainName                       # FQDN du serveur minio
+# parametres de log
+# ----------------
+#debug => {httpd=debug, nextcloud=0}      info => {httpd=info, nextcloud=1}       error => {httpd=error, nextcloud=3} 
+loglevel_httpd=$loglevel
+loglevel_nextcloud=1
+if [ "$loglevel" == "debug" ]; then loglevel_nextcloud=0; fi
+if [ "$loglevel" == "info" ];  then loglevel_nextcloud=1; fi
+if [ "$loglevel" == "error" ]; then loglevel_nextcloud=3; fi
+echo "loglevel_httpd = $loglevel_httpd"
+echo "loglevel_nextcloud = $loglevel_nextcloud"
+
+
+
+cd /tmp
+
+
+
+# install apache wer server (httpd)
+# ---------------------------------
+dnf install -y httpd httpd-tools mod_ssl 
+systemctl enable httpd
+systemctl start httpd
+systemctl status httpd
+
+
+# install php
+# ---------------
+dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+dnf install -y yum-utils wget sshpass
+dnf install -y php php-zip php-intl php-mysqlnd php-dom php-simplexml php-xml php-xmlreader
+dnf install -y php-curl php-exif php-ftp php-gd php-iconv php-json php-ldap php-mbstring php-posix php-sockets php-tokenizer php-opcache
+php -v
+
+
+
+##### CONFIGURE CONNEXION TO MARIA DB
+
+
+
+
+# install nextcloud
+# -----------------
+systemctl stop httpd
+
+# download NextCloud
+wget https://download.nextcloud.com/server/releases/nextcloud-24.0.3.zip
+
+# extract file into the folder /var/www/html/
+unzip nextcloud-24.0.3.zip -d /var/www/html/
+
+# Create a directory to store the user data,
+mkdir -p /var/www/html/nextcloud/data
+
+# Enable permission for the Apache webserver user to access the NextCloud files
+chown -R apache:apache /var/www/html/nextcloud/
+
+# restart apache
+systemctl restart httpd
+
+
+# OLD METHOD (sshpass)
+# ----------
+# On  recupere l'acces KEY/SECRET pour minio dans le fichier /root/minioTokenForNextcloud sur le serveur minio
+# full_line=`sshpass -p $minio_root_password ssh -o StrictHostKeyChecking=no root@$minio_server_IP 'cat /root/minioTokenForNextcloud'`
+# echo "full_line = $full_line"
+# ACCESS_KEY=`echo $full_line | awk '{print $3}'`
+# ACCESS_SECRET=`echo $full_line | awk '{print $6}'`
+# echo $ACCESS_KEY
+# echo $ACCESS_SECRET
+
+
+# On recupere sur le redis les key access/secret du minio
+# -------------------------------------------------------
+dnf install -y redis
+redis_auth=" -h $redisServer -p $redisPort --user dbadmin --pass $redis_password "
+cmd1=" get  Minio_Access_Key_$minio_server_Hostname " 
+ACCESS_KEY=`redis-cli $redis_auth $cmd1`
+cmd2=" get  Minio_Secret_Key_$minio_server_Hostname " 
+ACCESS_SECRET=`redis-cli $redis_auth $cmd2`
+
+
+
+
+
+# Config de l'acces au minio en https
+# -----------------------------------
+
+# Pour que nextcloud puisse communiquer avec Minio, il faut :
+#  1/ Dans /var/www/html/nextcloud/config/config.php:
+#     'hostname' => 'MINIO_SERVER',     =>  OUI: hostname  (PAS IP sinon pb avec certif en https)
+#     'port' =>  '9000'                 =>  OUI (9000 est aussi le port API et pas 40149)
+#     'use_ssl' => true,                =>  OUI pour HTTPS
+#  2/ Ajouter le certificat (public.crt) dans:  /var/www/html/nextcloud/resources/config/ca-bundle.crt
+# Download the self-signed certif from minio website:
+< /dev/null openssl s_client -connect $minio_FQDN:9000  | openssl x509 > /var/www/html/nextcloud/resources/config/certifminio.crt
+# update du fichier dans lequel on met les certifs utilisés par nextcloud
+echo " " >> /var/www/html/nextcloud/resources/config/ca-bundle.crt
+echo "Serveur minio pour nextcloud" >> /var/www/html/nextcloud/resources/config/ca-bundle.crt
+echo "============================" >> /var/www/html/nextcloud/resources/config/ca-bundle.crt
+cat /var/www/html/nextcloud/resources/config/certifminio.crt >> /var/www/html/nextcloud/resources/config/ca-bundle.crt
+
+# Pour que le code PHP (donc le systeme) puisse acceder en HTTPS il faut mettre a jour: /etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem  (Never upadate manually)
+# Il est fortement deconseillé d'ecrire directement ce fichier , il faut placer le certif dans '/usr/share/pki/ca-trust-source/anchors/' et faire 'update-ca-trust'
+cp /var/www/html/nextcloud/resources/config/certifminio.crt  /usr/share/pki/ca-trust-source/anchors/
+update-ca-trust
+
+# config acces minio
+cat <<EOF > /var/www/html/nextcloud/config/config.php
+<?php
+	\$CONFIG = array (
+     'objectstore' => [
+        'class' => '\\\OC\\\Files\\\ObjectStore\\\S3',
+        'arguments' => [
+                'bucket' => 'nextcloud',
+                'autocreate' => false,
+                'hostname' => 'MINIO_SERVER',
+                'key' => 'MINIO_KEY',
+                'secret' => 'MINIO_SECRET',
+                'port' => 9000,
+                'use_ssl' => true,
+                // required for some non-Amazon S3 implementations
+                'use_path_style' => true,
+        ],
+     ],
+	);
+EOF
+sed -i -e 's/MINIO_SERVER/'"$minio_FQDN"'/g'  /var/www/html/nextcloud/config/config.php
+sed -i -e 's/MINIO_KEY/'"$ACCESS_KEY"'/g'  /var/www/html/nextcloud/config/config.php
+sed -i -e 's/MINIO_SECRET/'"$ACCESS_SECRET"'/g'  /var/www/html/nextcloud/config/config.php
+
+
+
+
+
+# Enable permission for the Apache webserver user to access the NextCloud files
+chown -R apache:apache /var/www/html/nextcloud/
+
+# changement du loglevel de httpd ('LogLevel warn' par defaut)
+sed -i -e 's/LogLevel warn/LogLevel '"$loglevel_httpd"'/g'  /etc/httpd/conf/httpd.conf
+
+# restart apache
+systemctl start httpd
+
+
+
+
+# Install et config nextcloud  (equivalent a ce qui se passe lorsqu'on se connecte pour la premiere fois )
+# ----------------------------
+# https://docs.nextcloud.com/server/latest/admin_manual/configuration_server/occ_command.html#command-line-installation
+#sudo -u apache php /var/www/html/nextcloud/occ
+#sudo -u apache php /var/www/html/nextcloud/occ maintenance:install --help
+
+
+sudo -u apache php /var/www/html/nextcloud/occ maintenance:install \
+   --admin-user='admin' \
+   --admin-pass=$nextcloud_admin_password \
+   --database-host='localhost' \
+   --database='mysql' \
+   --database-name='nextcloud_db' \
+   --database-user='nextcloud-user' \
+   --database-pass=$DB_nextcloud_user_password \
+   --data-dir='/var/www/html/nextcloud/data/' 
+
+
+# ajout des URL autorisées pour les connexions
+#sudo -u apache php /var/www/html/nextcloud/occ  config:system:get  nextcloud  trusted_domain
+sudo -u apache php /var/www/html/nextcloud/occ  config:system:set   trusted_domains  1 --value=$nextcloud_IP
+sudo -u apache php /var/www/html/nextcloud/occ  config:system:set   trusted_domains  2 --value=$nextcloud_FQDN
+
+
+# Configurer le log level de nextcloud 
+# loglevel : 0=DEBUG(All activity), 1=INFO, 2=WARN, 3=ERROR, 5=FATAL
+#sudo -u apache php /var/www/html/nextcloud/occ  config:system:set   loglevel --value='1'
+sudo -u apache php /var/www/html/nextcloud/occ  config:system:set   loglevel --value=$loglevel_nextcloud
+
+
+
+
+# inutile desormais car install et conf effectuée
+rm -f /var/www/html/nextcloud/config/CAN_INSTALL
+
+
+# Creation du raccourci vers le fichier de log dans /var/log/
+ln -s  /var/www/html/nextcloud/data/nextcloud.log  /var/log/nextcloud.log
+
+
+
+
+# Configuration HTTPS dans Apache
+# -------------------------------
+# path vers le certificat SSL d'apache:
+# grep SSLCertificateFile /etc/httpd/conf.d/ssl.conf | grep -v "#"
+#    =>   SSLCertificateFile /etc/pki/tls/certs/localhost.crt
+#    =>   par defaut apache httpd recherche un /etc/pki/tls/certs/localhost.crt et la clé privée dans /etc/pki/tls/private/localhost.key
+
+# voir date d'expiration
+# openssl x509 -enddate -noout -in  /etc/pki/tls/certs/localhost.crt
+
+# Generate a self-signed certificate 
+cd /etc/pki/tls/certs/
+openssl req -x509 -sha256 -nodes -days 3650 -newkey rsa:2048 -keyout privateKey.key -out certificate.crt -subj "/C=XX/ST=FR/L=Paris/O=Broadcom/OU=CMPSE/CN=$nextcloud_FQDN"
+# on renomme et on met au bon endroit la clé privée
+mv certificate.crt localhost.crt
+mv privateKey.key /etc/pki/tls/private/localhost.key
+
+# on redirige les requetes entrantes HTTP en HTTPS  (ip et fqdn)
+cat <<EOF > /etc/httpd/conf.d/redirect_http.conf
+<VirtualHost *:80>
+   ServerName HOSTNAME
+   Redirect permanent / https://FQDN/
+</VirtualHost>
+EOF
+sed -i -e 's/HOSTNAME/'"$HOSTNAME"'/g'  /etc/httpd/conf.d/redirect_http.conf
+sed -i -e 's/FQDN/'"$nextcloud_FQDN"'/g'  /etc/httpd/conf.d/redirect_http.conf
+
+# restart apache
+systemctl restart httpd
+
+
+
+
+
+# nettoyage
+rm -f /var/www/html/nextcloud/resources/config/certifminio.crt
+dnf remove -y redis    # plus besoin
+
+
+
+
+
+
+# IDEE D'AMELIORATION
+#
+# - separer la DB  2-tiers => 3tiers
+#
+# - Attendre la fin d'execution du script cloud init avec de marquer comme fini ??   vis une clé sur redis ?
+#
+# - login de Minio aussi
+#
+# - integration dans Log Insight
+#
+
+
+
+
+
