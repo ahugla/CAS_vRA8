91c6baa9d7485cee2c6e1f4b19b3401f7e7b2ebb
ahugla <ahugla@vmware.com>
2024-01-16 14:20:14 +0100

    Create install_mariaDB_for_nextcloud.sh

---
+++ blueprints/nextcloud_with_minio_3tiers/install_mariaDB_for_nextcloud.sh
@@ -0,0 +1,68 @@
+#   INSTALLATION DE mariaDB sur Rocky Linux
+#
+#  v1.0
+#
+#  Alexandre Hugla
+#  16/01/2024
+#
+#
+#  USAGE :  install_mariaDB.sh [password]
+#
+
+
+
+# Recuperation des variables
+DB_root_password=$1   	
+
+
+cd /tmp/
+
+
+# install mariadb and configure it
+# --------------------------------
+dnf install -y mariadb-server mariadb
+systemctl enable mariadb
+systemctl start mariadb
+systemctl status mariadb
+
+# set password for 'root'
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_root_password';"
+mysql -e "FLUSH PRIVILEGES;"
+systemctl restart mariadb
+
+#mysql -u root -p
+#MariaDB [(none)]> SHOW DATABASES;
+#+--------------------+
+#| Database           |
+#+--------------------+
+#| information_schema |
+#| mysql              |
+#| performance_schema |
+#+--------------------+
+#4 rows in set (0.00 sec)
+
+# create nextcloud_db database for nextcloud
+mysql -u root  --password=$DB_root_password -e  "CREATE DATABASE nextcloud_db;"
+#mysql -u root -p
+#MariaDB [(none)]> SHOW DATABASES;
+#+--------------------+
+#| Database           |
+#+--------------------+
+#| information_schema |
+#| mysql              |
+#| nextcloud_db       |
+#| performance_schema |
+#+--------------------+
+#4 rows in set (0.00 sec)
+
+#Create a User called nextcloud-user and grant permissions on the nextcloud_db database
+mysql -u root  --password=$DB_root_password -e "GRANT ALL ON nextcloud_db.* TO 'nextcloud-user'@'localhost' IDENTIFIED BY '$DB_nextcloud_user_password';"
+mysql -u root  --password=$DB_root_password -e "FLUSH PRIVILEGES;"
+
+# Enable remote connection
+sed -i '2 i\bind-address = 0.0.0.0' /etc/my.cnf
+
+
+systemctl restart mariadb
+
+
