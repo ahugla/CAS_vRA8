a4fa6dd4a9c2ba261f647bf829b898d8e7144916
ahugla <ahugla@vmware.com>
2025-11-03 17:52:28 +0100

    lldap update

--- v9/all-apps/lldap/install-lldap.sh
+++ v9/all-apps/lldap/install-lldap.sh
@@ -6,14 +6,16 @@
 #     ------------------------------------
 #     https://github.com/lldap/lldap?tab=readme-ov-file
 #
-#     usage :  Web interface sur port 17170 
-#              compte "admin" / "password"
+#     usage: ./install-lldap.sh [adminPassword]
+#
+#     Web access :  Web interface sur port 17170 
+#                   compte "admin" / "default password"
 #     
 
 
 # Set parameters
-jwt_secret=VMware1!
-ldap_user_pass=VMware1!
+#jwt_secret=KKKKKK
+ldap_user_pass=$1   # adminPassword,  password for the 'admin' account on the web interface
 
 
 cd /tmp
@@ -27,17 +29,22 @@ dnf install git wget -y
 git clone https://github.com/lldap/lldap.git
 
 
-# update “jwt_secret” et “ldap_user_pass” dans /data/lldap_config.toml
+# Copie du fichier de config
 mkdir /data
 cp  lldap/lldap_config.docker_template.toml      /data/lldap_config.toml
 # If the lldap_config.toml doesn't exist when starting up, LLDAP will use default one. 
-# The default admin password is password, you can change the password later using the web interface.
+# On n'utilise pas le fichier de config pour updater le token jwt et le password admin (on le fait plus tard par variable d'env)
 
 
 # get docker compose file
 # fixe les variables d'env notamment pour 'jwt_secret' et 'ldap_user_pass'
 cd /data
 wget https://raw.githubusercontent.com/ahugla/CAS_vRA8/refs/heads/master/v9/all-apps/lldap/docker-compose.yaml --no-check-certificate
+
+
+# update “ldap_user_pass” (adminPassword) dans /data/lldap_config.toml
+# On passe par les variables d'environnement du docker compose file pour communiquer le password
+sed -i -e "s/AAAAAAAA"\"$ldap_user_pass\""/g"   /data/docker-compose.yaml
 
 
 # start lldap container
