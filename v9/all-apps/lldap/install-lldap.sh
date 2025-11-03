5fbf7de4d0b5f8be6ac0743118775696d8fb4b18
ahugla <ahugla@vmware.com>
2025-11-03 18:33:06 +0100

    Update install-lldap.sh

--- v9/all-apps/lldap/install-lldap.sh
+++ v9/all-apps/lldap/install-lldap.sh
@@ -33,18 +33,18 @@ git clone https://github.com/lldap/lldap.git
 mkdir /data
 cp  lldap/lldap_config.docker_template.toml      /data/lldap_config.toml
 # If the lldap_config.toml doesn't exist when starting up, LLDAP will use default one. 
-# On n'utilise pas le fichier de config pour updater le token jwt et le password admin (on le fait plus tard par variable d'env)
+# On peut mettre en dur les variables 'jwt_secret' et 'ldap_user_pass'.
+# Ici, on n'utilise pas le fichier de config pour updater le token jwt_secret et le password admin (on le fait plus tard par variable d'env)
 
 
 # get docker compose file
-# fixe les variables d'env notamment pour 'jwt_secret' et 'ldap_user_pass'
 cd /data
 wget https://raw.githubusercontent.com/ahugla/CAS_vRA8/refs/heads/master/v9/all-apps/lldap/docker-compose.yaml --no-check-certificate
 
 
 # update “ldap_user_pass” (adminPassword) dans /data/lldap_config.toml
 # On passe par les variables d'environnement du docker compose file pour communiquer le password
-sed -i -e "s/AAAAAAAA"\"$ldap_user_pass\""/g"   /data/docker-compose.yaml
+sed -i -e 's/AAAAAAAA/'"$ldap_user_pass"'/g'   /data/docker-compose.yaml
 
 
 # start lldap container
